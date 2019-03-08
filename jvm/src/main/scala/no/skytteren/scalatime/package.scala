a69b6fdf0e188f6ea14e024026b5150416a81458
Stein Kåre Skytteren <sk@mimewell.no>
2019-03-08 15:43:36 +0100

    Adding DateTime.now pimp

--- jvm/src/main/scala/no/skytteren/scalatime/package.scala
+++ jvm/src/main/scala/no/skytteren/scalatime/package.scala
@@ -1,5 +1,7 @@
 package no.skytteren
 
+import java.time.LocalDateTime
+
 package object scalatime {
 
   implicit class DatePimp(date: Date.type) {
@@ -13,6 +15,16 @@ package object scalatime {
     def now(): Time = {
       val jTime = java.time.LocalTime.now()
       new Time(Hour(jTime.getHour), Minute(jTime.getMinute), Second(jTime.getSecond), Millisecond(jTime.getNano / 1000000))
+    }
+  }
+
+  implicit class DateTimePimp(dateTime: DateTime.type) {
+    def now(): DateTime = {
+      val jDateTime = LocalDateTime.now()
+      new DateTime(
+        Year(jDateTime.getYear), Month(jDateTime.getMonthValue), DayOfMonth(jDateTime.getDayOfMonth),
+        Hour(jDateTime.getHour), Minute(jDateTime.getMinute), Second(jDateTime.getSecond), Millisecond(jDateTime.getNano / 1000000)
+      )
     }
   }
 }
